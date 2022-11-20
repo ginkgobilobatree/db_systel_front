5f2f5f436d8c6613934b536a353a849d7738861c
Gino Massalski <gino.massalski@gmail.com>
2022-11-20 23:10:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./img/DB-Systel-gmbh-logo.png";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [input, setInput] = useState({station1:"", station2: ""});
+
+  const handleChange = e => {
+    const value = e.target.value;
+    setInput({
+      ...input,
+      [e.target.name]: value
+    })
+  };
+
+  const submit = async () => {
+    const response = await fetch(`/api/v1/distance/${input.station1}/${input.station2}`);
+    const result = await response.json();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section>
+      <figure>
+        <figcaption></figcaption>
+        <img src={Logo} alt="logo db systel gmbh" />
+      </figure>
+      <form id="form" onSubmit={submit}>
+          <input id="inputField" type="text" placeholder="Station 1" name="station1" value={input.station1} onChange={handleChange} />
+          <input id="inputField" type="text" placeholder="Station 2" name="station2" value={input.station2} onChange={handleChange} />
+        <input id="send" type="submit" value="Berechne Distanz"  />
+      </form>
+      <p>{input.station1}</p>
+      <p>{input.station2}</p>
+      </section>
+    </main>
   );
 }
-
-export default App;
