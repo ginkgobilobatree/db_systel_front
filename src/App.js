c60b50339e9e19d5d6a6d3fce39915144faf81ec
Gino Massalski <gino.massalski@gmail.com>
2022-11-21 12:05:59 +0100

    fetch data from backend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 export default function App() {
   const [input, setInput] = useState({station1:"", station2: ""});
+  const [answer, setAnswer] = useState(["test", {station2: "hello"}]);
 
   const handleChange = e => {
     const value = e.target.value;
@@ -13,9 +14,19 @@ export default function App() {
     })
   };
 
-  const submit = async () => {
-    const response = await fetch(`/api/v1/distance/${input.station1}/${input.station2}`);
+  const submit = async (e) => {
+    e.preventDefault();
+    const req = {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+    const response = await fetch(`/api/v1/distance/${input.station1}/${input.station2}`, req);
     const result = await response.json();
+    console.log(result)
+    setAnswer(result);
+    setInput({station1:"", station2: ""});
   }
 
   return (
@@ -25,10 +36,10 @@ export default function App() {
         <figcaption></figcaption>
         <img src={Logo} alt="logo db systel gmbh" />
       </figure>
-      <form id="form" onSubmit={submit}>
+      <form id="form">
           <input id="inputField" type="text" placeholder="Station 1" name="station1" value={input.station1} onChange={handleChange} />
           <input id="inputField" type="text" placeholder="Station 2" name="station2" value={input.station2} onChange={handleChange} />
-        <input id="send" type="submit" value="Berechne Distanz"  />
+        <input id="send" type="submit" value="Berechne Distanz" onClick={submit} />
       </form>
       <p>{input.station1}</p>
       <p>{input.station2}</p>
