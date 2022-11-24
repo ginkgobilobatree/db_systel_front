5e85fa2de1328c5648924e404efc8213d5483edc
Gino Massalski <gino.massalski@gmail.com>
2022-11-24 14:02:27 +0100

    use try/catch, change value for setResult, delete drop-shadow

--- src/utils/submit.js
+++ src/utils/submit.js
@@ -1,8 +1,13 @@
 export const submit = async (e, input, setInput, setResult) => {
+  const invalidInput = {
+    from: "invalid input",
+    to: "invalid input",
+    distance: "invalid input",
+    unit: "invalid input",
+  };
+
   e.preventDefault();
-  if (input.station1 === "" || input.station2 === "") {
-    alert("please, fill out both input fields correctly.");
-  }
+
   const req = {
     method: "GET",
     headers: {
@@ -10,12 +15,23 @@ export const submit = async (e, input, setInput, setResult) => {
     },
   };
 
-  const response = await fetch(
-    `/api/v1/distance/${input.station1}/${input.station2}`,
-    req
-  );
-  const result = await response.json();
-  console.log(result);
-  setResult(result);
-  setInput({ station1: "", station2: "" });
+  try {
+    const response = await fetch(
+      `/api/v1/distance/${input.station1}/${input.station2}`,
+      req
+    );
+    const result = await response.json();
+    if (input.station1 === "" || input.station2 === "") {
+      alert("please, fill out both input fields.");
+      console.log(invalidInput); // result ist not working => it would give the respond for the wrong endpoint
+      setResult(invalidInput);
+      setInput({ station1: "", station2: "" });
+    } else {
+      console.log(result);
+      setResult(result);
+      setInput({ station1: "", station2: "" });
+    }
+  } catch (error) {
+    throw error;
+  }
 };
